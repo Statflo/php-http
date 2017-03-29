a415f7073fe9f323ae25ab72aaba731bbb25ecee
Kinn Coelho Juliao <kinncj@php.net>
2017-03-29 13:32:18 -0400

    Fixed trait reference

--- src/Statflo/HTTP/Controller/ContainerController.php
+++ src/Statflo/HTTP/Controller/ContainerController.php
@@ -4,8 +4,9 @@ namespace Statflo\HTTP\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Statflo\DI\Base\ContainerAware;
 use Statflo\HTTP\DTO\Response;
+use Statflo\HTTP\ResponseTrait;
 
 abstract class ContainerController extends ContainerAware
 {
-    use Statflo\HTTP\ResponseTrait;
+    use ResponseTrait;
 }
